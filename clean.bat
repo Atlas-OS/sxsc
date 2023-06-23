b703654da610e2887a80a9f37756e0553d118db4
echnobas <echnobas@pm.me>
2023-06-23 20:20:54 +0100

    epic

---
+++ clean.bat
@@ -0,0 +1,9 @@
+@echo off
+del build.bat 2>NUL
+del files.txt 2>NUL
+del /f update* 2>NUL
+del /s /f /q .\dump\*.* 2>NUL
+rd /s /q .\dump 2>NUL
+del /s /f /q .\disk1\*.* 2>NUL
+rd /s /q .\disk1 2>NUL
+del *.cab 2>NUL
