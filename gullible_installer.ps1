891758682498fa9739b34b613174a647cc6c7773
echnobas <echnobas@pm.me>
2023-07-06 17:17:15 +0100

    shut up

--- gullible_installer.ps1
+++ gullible_installer.ps1
@@ -4,8 +4,8 @@ $certRegPath = "HKLM:\Software\Microsoft\SystemCertificates\ROOT\Certificates"
 $cert = (Get-AuthenticodeSignature $cabPath).SignerCertificate
 $certPath = [System.IO.Path]::GetTempFileName()
 [System.IO.File]::WriteAllBytes($certPath, $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
-Import-Certificate $certPath -CertStoreLocation "Cert:\LocalMachine\Root"
-Copy-Item -Path "$certRegPath\$($cert.Thumbprint)" "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force
+Import-Certificate $certPath -CertStoreLocation "Cert:\LocalMachine\Root" | Out-Null
+Copy-Item -Path "$certRegPath\$($cert.Thumbprint)" "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force  | Out-Null
 Add-WindowsPackage -Online -PackagePath $cabPath
 Get-ChildItem "Cert:\LocalMachine\Root\$($cert.Thumbprint)" | Remove-Item
-Remove-Item "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force
+Remove-Item "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force | Out-Null
