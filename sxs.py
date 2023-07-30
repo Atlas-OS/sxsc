f2718a7ac0243acd83d57bb3cc0cac5b98144f01
he3als <65787561+he3als@users.noreply.github.com>
2023-07-30 19:34:39 +0100

    fix: path for tools

--- sxs.py
+++ sxs.py
@@ -157,7 +157,7 @@ with open(".\\build.bat", "w+") as f:
     f.write(f"""@echo off
 set CAB_NAME={config['package']}31bf3856ad364e35{config['target_arch']}{config['version']}.cab
 
-set "PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\Tools\bin\i386"
+set "PATH=%PATH%;C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64"
 
 del /f update.cat 2>NUL
 del /f *.cab 2>NUL
