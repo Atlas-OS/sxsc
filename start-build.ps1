a351a94f0feeab200032a41d7577bd150b0c05e2
he3als <65787561+he3als@users.noreply.github.com>
2024-07-14 10:14:41 +0100

    Delete start-build.ps1

--- start-build.ps1
+++
@@ -1,9 +0,0 @@
-param (
-	[Parameter( Mandatory = $True )]
-	[string]$Thumbprint
-)
-
-.\build.ps1 -Thumbprint $Thumbprint -CabName 'Z-Atlas-NoDefender-Package31bf3856ad364e35amd643.0.0.0.cab'
-
-Write-Output "Deleting temp folder..."
-Remove-Item -Path 'C:\Users\he3als\AppData\Local\Temp\tmpi0ytoowp' -Force -Recurse
